ac78497d148a8e7362e2c0bb98a6093f5f3a54d5
kod2nd <colinongengkiat@gmail.com>
2018-09-05 22:58:06 +0800

    added seedData

---
+++ src/utils/seedData.js
@@ -0,0 +1,24 @@
+const seedData = [
+    {
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/7202",
+      title: "Chicken Fettuccini Alfredo",
+      source_url:
+        "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx",
+      recipe_id: "7202",
+      image_url: "http://static.food2fork.com/214411de5a.jpg",
+      social_rank: 99.9999995711024,
+      publisher_url: "http://allrecipes.com"
+    },
+    {
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/5427",
+      title: "Cajun Chicken Pasta",
+      source_url:
+        "http://allrecipes.com/Recipe/Cajun-Chicken-Pasta-2/Detail.aspx",
+      recipe_id: "5427",
+      image_url: "http://static.food2fork.com/16462cc7b.jpg",
+      social_rank: 99.99984174656116,
+      publisher_url: "http://allrecipes.com"
+    }
+  ];
