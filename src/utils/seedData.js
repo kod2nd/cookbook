f4c9fb395f08d15c341c1835bc89f9063a40094e
kod2nd <colinongengkiat@gmail.com>
2018-09-06 00:49:40 +0800

    added scrollbar to list

--- src/utils/seedData.js
+++ src/utils/seedData.js
@@ -20,7 +20,72 @@ const seedData = [
       image_url: "http://static.food2fork.com/16462cc7b.jpg",
       social_rank: 99.99984174656116,
       publisher_url: "http://allrecipes.com"
-    }
+    },
+    {
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/7202",
+      title: "Chicken Fettuccini Alfredo",
+      source_url:
+        "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx",
+      recipe_id: "7502",
+      image_url: "http://static.food2fork.com/214411de5a.jpg",
+      social_rank: 99.9999995711024,
+      publisher_url: "http://allrecipes.com"
+    },
+    {
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/7202",
+      title: "Chicken Fettuccini Alfredo",
+      source_url:
+        "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx",
+      recipe_id: "7262",
+      image_url: "http://static.food2fork.com/214411de5a.jpg",
+      social_rank: 99.9999995711024,
+      publisher_url: "http://allrecipes.com"
+    },
+    {
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/7202",
+      title: "Chicken Fettuccini Alfredo",
+      source_url:
+        "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx",
+      recipe_id: "7212",
+      image_url: "http://static.food2fork.com/214411de5a.jpg",
+      social_rank: 99.9999995711024,
+      publisher_url: "http://allrecipes.com"
+    },
+    {
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/7202",
+      title: "Chicken Fettuccini Alfredo",
+      source_url:
+        "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx",
+      recipe_id: "7207",
+      image_url: "http://static.food2fork.com/214411de5a.jpg",
+      social_rank: 99.9999995711024,
+      publisher_url: "http://allrecipes.com"
+    },
+    {
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/7202",
+      title: "Chicken Fettuccini Alfredo",
+      source_url:
+        "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx",
+      recipe_id: "7203",
+      image_url: "http://static.food2fork.com/214411de5a.jpg",
+      social_rank: 99.9999995711024,
+      publisher_url: "http://allrecipes.com"
+    },{
+      publisher: "All Recipes",
+      f2f_url: "http://food2fork.com/view/7202",
+      title: "Chicken Fettuccini Alfredo",
+      source_url:
+        "http://allrecipes.com/Recipe/Chicken-Fettuccini-Alfredo/Detail.aspx",
+      recipe_id: "9202",
+      image_url: "http://static.food2fork.com/214411de5a.jpg",
+      social_rank: 99.9999995711024,
+      publisher_url: "http://allrecipes.com"
+    },
   ];
 
   export default seedData
