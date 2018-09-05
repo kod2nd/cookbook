73e9bca7005de507da5a1bfad77dec35e00499a5
kod2nd <colinongengkiat@gmail.com>
2018-09-05 17:22:27 +0800

    added classnames to App child comonents

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Body />
+        <Header className="header"/>
+        <Body className="body"/>
       </div>
     );
   }
