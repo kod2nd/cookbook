d5dbc4427cd724f533d4c6bce1ed381c960ba36a
kod2nd <colinongengkiat@gmail.com>
2018-09-07 00:41:44 +0800

    removed commented out api key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './component/Header';
 import Body from './component/Body';
-// const API_KEY = process.env.REACT_APP_FORK_2_FORK_API_KEY
+
 
 
 class App extends Component {
