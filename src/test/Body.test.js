b045a39c1e4e9d3493a701ce2aca392738c47075
kod2nd <colinongengkiat@gmail.com>
2018-09-06 20:27:02 +0800

    tested setSelectedItem in Body.js

--- src/test/Body.test.js
+++ src/test/Body.test.js
@@ -1,21 +1,30 @@
-import React from "react"
+import React from "react";
 import Body from "../component/Body";
 import { shallowRender } from "./helper/testHelper";
 import { shallow } from "enzyme";
 import SideBar from "../component/SideBar";
-import Display from "../component/Display"
+import Display from "../component/Display";
+
+const wrapper = shallow(<Body />);
 
 describe("<Body />", () => {
-  const wrapper = shallow(<Body />)
-  describe('renders component', () => {
-    it('<SideBar />', () => {
-      expect(wrapper.find(SideBar)).toHaveLength(1)
+  describe("renders component", () => {
+    it("<SideBar />", () => {
+      expect(wrapper.find(SideBar)).toHaveLength(1);
     });
-    it('<Display />', () => {
-      expect(wrapper.find(Display)).toHaveLength(1)
+    it("<Display />", () => {
+      expect(wrapper.find(Display)).toHaveLength(1);
     });
-    
   });
+
+  describe("Methods", () => {
+    it("setSelectedItem should set the state of selectedItem with the argument that is passed into it", () => {
+      const TEST_RECIPE_ID = 12345;
+      wrapper.instance().setSelectedItem(TEST_RECIPE_ID);
+      expect(wrapper.state("selectedItem")).toEqual(12345);
+    });
+  });
+
   describe("Snapshot", () => {
     const result = shallowRender(Body);
     it("should match", () => {
