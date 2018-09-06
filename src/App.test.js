15ca23db8a6aaa289528f8bfe0381671c952cdda
kod2nd <colinongengkiat@gmail.com>
2018-09-06 11:17:37 +0800

    updated App test to use Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Header from "./component/Header"
-import {shallowRender, isInArray} from "./test/helper/testHelper"
-import { shallow, mount, render } from 'enzyme';
-
-const result = shallowRender(App)
-
-test('should ', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.exists()).toBe(true)
-});
-
-test('Should render header', () => {
-  const wrapper = shallow(<App />);
-  console.log(wrapper.find(Header))
-  expect(wrapper.find(Header)).toHaveLength(1)
-});
+import Header from "./component/Header";
+import Body from "./component/Body";
+import { shallow } from "enzyme";
 
 describe("<App />", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-  });
-
-  it("Has Header and Body Components", () => {
-    const components = result.props.children.map(child => {
-      return child.type.name;
+  const wrapper = shallow(<App />);
+  describe("renders", () => {
+    it("<Header />", () => {
+      expect(wrapper.find(Header)).toHaveLength(1);
     });
-    expect(isInArray("Header", components)).toBe(true);
-    expect(isInArray("Body", components)).toBe(true);
+    it("<Body />", () => {
+      expect(wrapper.find(Body)).toHaveLength(1);
+    });
   });
 });
