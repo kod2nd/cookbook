f6feae0663ecd5db4776ab0f5895d1a650e301f8
kod2nd <colinongengkiat@gmail.com>
2018-09-07 11:07:08 +0800

    updated modal tests

--- src/test/Modal.test.js
+++ src/test/Modal.test.js
@@ -10,10 +10,13 @@ describe("<Modal /> has", () => {
     it("view-recipe", () => {
       expect(wrapper.find(".view-recipe")).toHaveLength(1);
     });
-  it("modalDialog", () => {
+  it("a modal", () => {
     expect(wrapper.find(".modal-popup")).toHaveLength(1);
   });
-//   it("<Display />", () => {
-//     expect(wrapper.find(Display)).toHaveLength(1);
-//   });
+  it("a modal content", () => {
+    expect(wrapper.find(".modal-content")).toHaveLength(1);
+  });
+  it("an iframe", () => {
+    expect(wrapper.find(".iframe")).toHaveLength(1);
+  });
 });
